8f1e5b2878c96dcdb645f077614db9389f65bef0
Victor212-hub <joaoarouca212@gmail.com>
2026-05-26 22:28:32 -0300

    feat altera a navegação do login para usar eventos React

--- sistema-matricula-react/src/App.jsx
+++ sistema-matricula-react/src/App.jsx
@@ -1,62 +1,28 @@
-import logo from './assets/logo.png.png';
-import './styles/login.css';
+import Login from './pages/Login'
+import{ useState} from 'react'
+import Login from './pages/Login'
 
 function App() {
-  return (
-    <main>
-      <div className="area_login">
+  const[paginalAtual, setPaginaAtual] = useState('login')
 
-        <div className="card_login">
-          <img src={logo} alt="Logo do Sistema de Matrícula" className="logo" />
+  if (paginaAtual === 'login') {
+    return <Login mudarPagina={setPaginaAtual} />
+  }
 
-          <h2>Acesso ao sistema</h2>
+  if (paginalAtual === 'coordenador') {
+    return <h1>Tela do Coordenador</h1>
+  }
 
-          {/* CONTROLES (abas) */}
-          <input type="radio" name="perfil" id="coord" defaultChecked />
-          <input type="radio" name="perfil" id="prof" />
-          <input type="radio" name="perfil" id="aluno" />
+  if (paginaAtual === 'professor') {
+    return <h1>Tela do Professor</h1>
+  }
 
-          <div className="tabs">
-            <label htmlFor="coord">Coordenador</label>
-            <label htmlFor="prof">Professor</label>
-            <label htmlFor="aluno">Aluno</label>
-          </div>
+  if (paginaAtual === 'aluno') {
+    return <h1>Tela do Aluno</h1>
+  }
 
-          {/* FORMULÁRIOS */}
+  return <Login mudarPagina={setPaginaAtual} />
+  }
 
-          <div className="formularios">
 
-            {/* Coordenador */}
-            <form className="form coord">
-              <input className="inputt" type="email" placeholder="Email institucional 📩" required />
-              <input className="inputt" type="password" placeholder="Senha 🔐" required />
-              <a href="tela_coordenador.html" className="botao">Entrar</a>
-            </form>
-
-            {/* Professor */}
-            <form className="form prof">
-              <input className="inputt" type="email" placeholder="Email 📩" required />
-              <input className="inputt" type="password" placeholder="Senha 🔐" required />
-              <a href="tela_professor.html" className="botao">Entrar</a>
-            </form>
-
-            {/* Aluno */}
-            <form className="form aluno">
-              <input className="inputt" type="number" placeholder="Matrícula 📄" required />
-              <input className="inputt" type="password" placeholder="Senha 🔐" required />
-              <a href="tela_aluno.html" className="botao">Entrar</a>
-            </form>
-
-          </div>
-
-        </div>
-
-      </div>
-
-      <footer>
-        <p>&copy; 2026 Sistema de Matrícula Escolar</p>
-      </footer>
-    </main>
-  )
-}
 export default App
---
+++ sistema-matricula-react/src/pages/Login.jsx
@@ -0,0 +1,62 @@
+import logo from '../assets/logo.png.png';
+import '../styles/login.css';
+
+function Login({ mudarPagina}) {
+  return (
+    <main>
+      <div className="area_login">
+
+        <div className="card_login">
+          <img src={logo} alt="Logo do Sistema de Matrícula" className="logo" />
+
+          <h2>Acesso ao sistema</h2>
+
+          {/* CONTROLES (abas) */}
+          <input type="radio" name="perfil" id="coord" defaultChecked />
+          <input type="radio" name="perfil" id="prof" />
+          <input type="radio" name="perfil" id="aluno" />
+
+          <div className="tabs">
+            <label htmlFor="coord">Coordenador</label>
+            <label htmlFor="prof">Professor</label>
+            <label htmlFor="aluno">Aluno</label>
+          </div>
+
+          {/* FORMULÁRIOS */}
+
+          <div className="formularios">
+
+            {/* Coordenador */}
+            <form className="form coord">
+              <input className="inputt" type="email" placeholder="Email institucional 📩" required />
+              <input className="inputt" type="password" placeholder="Senha 🔐" required />
+              <button className="botao" onClick={() => mudarPagina('coordenador')}>Entrar</button>
+            </form>
+
+            {/* Professor */}
+            <form className="form prof">
+              <input className="inputt" type="email" placeholder="Email 📩" required />
+              <input className="inputt" type="password" placeholder="Senha 🔐" required />
+              <button type = "button" onClick={() =>mudarPagina ('professor')} className="botao">Entrar</button>
+            </form>
+
+            {/* Aluno */}
+            <form className="form aluno">
+              <input className="inputt" type="number" placeholder="Matrícula 📄" required />
+              <input className="inputt" type="password" placeholder="Senha 🔐" required />
+              <button type = "button" onClick={() =>mudarPagina ('aluno')} className="botao">Entrar</button>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <footer>
+        <p>&copy; 2026 Sistema de Matrícula Escolar</p>
+      </footer>
+    </main>
+  )
+}
+export default Login
