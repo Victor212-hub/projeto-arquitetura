17aaebe56d522bc4fb0af93c082467a6c8d6c18b
Victor212-hub <joaoarouca212@gmail.com>
2026-05-26 21:11:40 -0300

    feat:mudança do html principal para o app.jsx

--- sistema-matricula-react/src/App.jsx
+++ sistema-matricula-react/src/App.jsx
@@ -1,122 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import logo from './assets/logo.png.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <main>
+      <div className="area_login">
 
-      <div className="ticks"></div>
+        <div className="card_login">
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+          <h2>Acesso ao sistema</h2>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+          {/* CONTROLES (abas) */}
+          <input type="radio" name="perfil" id="coord" checked />
+          <input type="radio" name="perfil" id="prof" />
+          <input type="radio" name="perfil" id="aluno" />
+
+          <div className="tabs">
+            <label htmlFor="coord">Coordenador</label>
+            <label htmlFor="prof">Professor</label>
+            <label htmlFor="aluno">Aluno</label>
+          </div>
+
+          {/* FORMULÁRIOS */}
+
+          <div className="formularios">
+
+            {/* Coordenador */}
+            <form className="form coord">
+              <input className="inputt" type="email" placeholder="Email institucional 📩" required />
+              <input className="inputt" type="password" placeholder="Senha 🔐" required />
+              <a href="tela_coordenador.html" className="botao">Entrar</a>
+            </form>
+
+            {/* Professor */}
+            <form className="form prof">
+              <input className="inputt" type="email" placeholder="Email 📩" required />
+              <input className="inputt" type="password" placeholder="Senha 🔐" required />
+              <a href="tela_professor.html" className="botao">Entrar</a>
+            </form>
+
+            {/* Aluno */}
+            <form className="form aluno">
+              <input className="inputt" type="number" placeholder="Matrícula 📄" required />
+              <input className="inputt" type="password" placeholder="Senha 🔐" required />
+              <a href="tela_aluno.html" className="botao">Entrar</a>
+            </form>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <footer>
+        <p>&copy; 2026 Sistema de Matrícula Escolar</p>
+      </footer>
+    </main>
   )
 }
-
 export default App
