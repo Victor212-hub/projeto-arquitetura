995e16a8588e3f7a3b4e91f987bc261529394585
Victor212-hub <joaoarouca212@gmail.com>
2026-05-30 17:55:38 -0300

    feat: adiciona autenticação com dados locais

---
+++ sistema-matricula-react/src/data/seedData.js
@@ -0,0 +1,30 @@
+export const usuariosIniciais = [
+  {
+    id: 1,
+    nome: 'Coordenador',
+    perfil: 'coordenador',
+    login: 'coord@escola.com',
+    senha: '1234',
+  },
+  {
+    id: 2,
+    nome: 'Professor',
+    perfil: 'professor',
+    login: 'prof@escola.com',
+    senha: '1234',
+  },
+  {
+    id: 3,
+    nome: 'João Silva',
+    perfil: 'aluno',
+    login: '2026001',
+    senha: '1234',
+  },
+  {
+    id: 4,
+    nome: 'Sérgio Oliveira',
+    perfil: 'aluno',
+    login: '2026002',
+    senha: '4263',
+  },
+]
